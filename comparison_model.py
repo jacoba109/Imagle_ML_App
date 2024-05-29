6c97937170fdd2365676dc6ecd511e0cabe5b3a4
jacob <jacobanderson109@gmail.com>
2024-05-29 18:51:03 -0400

    Updated comparison model with training code.

--- comparison_model.py
+++ comparison_model.py
@@ -15,5 +15,159 @@ from keras.api.applications import resnet
 
 target_shape = (200, 200)
 
+def preprocess_image(filename):
+    image_string = tf.io.read_file(filename)
+    image = tf.image.decode_jpeg(image_string, channels=3)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, target_shape)
+    return image
 
+def preprocess_triplets(anchor, positive, negative):
+    return (
+        preprocess_image(anchor), 
+        preprocess_image(positive), 
+        preprocess_image(negative)
+    )
 
+anchor_images = sorted([str("left/" + f) for f in os.listdir("left")])
+positive_images = sorted([str("right/" + f) for f in os.listdir("right")])
+image_count = len(anchor_images)
+
+anchor_dataset = tf.data.Dataset.from_tensor_slices(anchor_images)
+positive_dataset = tf.data.Dataset.from_tensor_slices(positive_images)
+
+rng = np.random.RandomState(seed=17)
+rng.shuffle(anchor_images)
+rng.shuffle(positive_images)
+
+negative_images = anchor_images + positive_images
+np.random.RandomState(seed=34).shuffle(negative_images)
+
+negative_dataset = tf.data.Dataset.from_tensor_slices(negative_images)
+negative_dataset = negative_dataset.shuffle(buffer_size=4096)
+
+dataset = tf.data.Dataset.zip((anchor_dataset, positive_dataset, negative_dataset))
+dataset = dataset.shuffle(buffer_size=1024)
+dataset = dataset.map(preprocess_triplets)
+
+train_dataset = dataset.take(round(image_count * 0.8))
+val_dataset = dataset.skip(round(image_count * 0.8))
+
+train_dataset = train_dataset.batch(32, drop_remainder=False)
+train_dataset = train_dataset.prefetch(tf.data.AUTOTUNE)
+
+val_dataset = val_dataset.batch(32, drop_remainder=False)
+val_dataset = val_dataset.prefetch(tf.data.AUTOTUNE)
+
+def visualize(anchor, positive, negative):
+
+    def show(ax, image):
+        ax.imshow(image)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+    fig = plt.figure(figsize=(9, 9))
+
+    axs = fig.subplots(3, 3)
+    for i in range(3):
+        show(axs[i, 0], anchor[i])
+        show(axs[i, 1], positive[i])
+        show(axs[i, 2], negative[i])
+
+base_cnn = resnet.ResNet50(weights="imagenet", input_shape = target_shape + (3,), include_top=False)
+flatten = layers.Flatten()(base_cnn.output)
+dense1 = layers.Dense(512, activation="relu")(flatten)
+dense1 = layers.BatchNormalization()(dense1)
+dense2 = layers.Dense(256, activation="relu")(dense1)
+dense2 = layers.BatchNormalization()(dense2)
+output = layers.Dense(256)(dense2)
+
+embedding = Model(base_cnn.input, output, name="Embedding")
+
+trainable = False
+for layer in base_cnn.layers:
+    if layer.name == "conv5_block1_out":
+        trainable = True
+    layer.trainable = trainable
+
+class DistanceLayer(layers.Layer):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, anchor, positive, negative):
+        ap_distance = ops.sum(tf.square(anchor - positive), -1)
+        an_distance = ops.sum(tf.square(anchor - negative), -1)
+        return (ap_distance, an_distance)
+    
+
+anchor_input = layers.Input(name="anchor", shape=target_shape + (3,))
+positive_input = layers.Input(name="positive", shape=target_shape + (3,))
+negative_input = layers.Input(name="negative", shape=target_shape + (3,))
+
+distances = DistanceLayer()(embedding(resnet.preprocess_input(anchor_input)), 
+                            embedding(resnet.preprocess_input(positive_input)), 
+                            embedding(resnet.preprocess_input(negative_input)))
+
+siamese_network = Model(inputs = [anchor_input, positive_input, negative_input], outputs=distances)
+
+class SiameseModel(Model):
+
+    def __init__(self, siamese_network, margin=0.5):
+        super().__init__()
+        self.siamese_network = siamese_network
+        self.margin = margin
+        self.loss_tracker = metrics.Mean(name="loss")
+
+    def call(self, inputs):
+        return self.siamese_network(inputs)
+    
+    def train_step(self, data):
+
+        with tf.GradientTape() as tape:
+            loss = self._compute_loss(data)
+
+        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+
+        self.optimizer.apply_gradients(zip(gradients, self.siamese_network.trainable_weights))
+
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+    
+    def test_step(self, data):
+        loss = self._compute_loss(data)
+
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+    
+    def _compute_loss(self, data):
+        ap_distance, an_distance = self.siamese_network(data)
+
+        loss = ap_distance - an_distance
+        loss = tf.maximum(loss + self.margin, 0.0)
+        return loss
+    
+    @property
+    def metrics(self):
+        return [self.loss_tracker]
+    
+siamese_model = SiameseModel(siamese_network)
+siamese_model.compile(optimizer=optimizers.Adam(0.0001))
+siamese_model.fit(train_dataset, epochs=10, validation_data=val_dataset)
+
+sample = next(iter(train_dataset))
+
+anchor, positive, negative = sample
+anchor_embedding, positive_embedding, negative_embedding = (embedding(resnet.preprocess_input(anchor)), 
+                                                            embedding(resnet.preprocess_input(positive)), 
+                                                            embedding(resnet.preprocess_input(negative)))
+
+cosine_similarity = metrics.CosineSimilarity()
+
+positive_similarity = cosine_similarity(anchor_embedding, positive_embedding)
+print("Positive similarity:", positive_similarity.numpy())
+
+negative_similarity = cosine_similarity(anchor_embedding, negative_embedding)
+print("Negative similarity:", negative_similarity.numpy())
+
+siamese_model.save("./comparison_model.keras")
